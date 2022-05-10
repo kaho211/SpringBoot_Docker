cca602121ff7f69f7ec65b8c03b860e3d697271d
kaho211 <kaho.fujii@tokyo.supersoftware.co.jp>
2022-05-11 00:54:53 +0900

    DB接続+UsersControllerの単体テスト

---
+++ server/src/test/java/com/example/app/CsvDataSetLoader.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import com.github.springtestdbunit.dataset.AbstractDataSetLoader;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.csv.CsvURLDataSet;
+import org.springframework.core.io.Resource;
+
+//CSVのファイルを取得しDB操作のテストで使用できるようにするクラス
+public class CsvDataSetLoader extends AbstractDataSetLoader {
+    @Override
+    protected IDataSet createDataSet(Resource resource) throws Exception {
+        // resourceオブジェクトにCSVファイルのパスが格納されている
+        return new CsvURLDataSet(resource.getURL());
+    }
+}
---
+++ server/src/test/java/com/example/app/users/UsersControllerUnitTests.java
@@ -0,0 +1,57 @@
+package com.example.app.users;
+
+// import com.example.practice.PracticeApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UsersControllerUnitTests {
+        // Mockオブジェクト
+        @Autowired
+        private MockMvc mockMvc;
+        private Object User;
+
+        @Test
+        // getリクエスト 問い合わせ画面
+        void index処理でviewとしてindexが渡される() throws Exception {
+                this.mockMvc.perform(get("/users/"))
+                                .andExpect(status().isOk())
+                                .andExpect(view().name("users/index"));
+        }
+
+        @Test
+        // getリクエスト 新規作成画面
+        void newUser処理でviewとしてnewが渡される() throws Exception {
+                this.mockMvc.perform(get("/users/new"))
+                                .andExpect(status().isOk())
+                                .andExpect(view().name("users/new"));
+        }
+
+        @Test
+        // Entityに納められたデータを引っ張ってこれているか
+        void フォームに値が入力されている場合は確認画面へ() throws Exception {
+                // ContactForm contactForm = new ContactForm();
+                // contactForm.setName("test");
+                // contactForm.setMail("test@email.com");
+                // contactForm.setContent("テストです。");
+
+                this.mockMvc
+                                .perform((post("/users/confirm"))
+                                                .param("name", "テスト")
+                                                .param("email", "test@test")
+                                                .param("inquiry", "テストです。"))
+                                .andExpect(status().isOk())
+                                .andExpect(model().hasNoErrors())
+                                .andExpect(model().attribute("User", User))
+                                .andExpect(view().name("users/confirm"));
+        }
+
+}
